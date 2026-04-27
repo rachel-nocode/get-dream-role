bb1188d595b0c4788bac151c2ad5eebea03419cb
witch audio <witchaudiostudios@gmail.com>
2026-04-27 10:48:20 -0700

    Enable DataFast pageview capture

--- src/components/analytics/DataFastAnalytics.tsx
+++ src/components/analytics/DataFastAnalytics.tsx
@@ -4,11 +4,14 @@ import { useEffect } from "react";
 import { initDataFast } from "datafast";
 
 const DATAFAST_WEBSITE_ID = "dfid_49LZW4H1LKiCeK8IfP5fp";
+const DATAFAST_DOMAIN = "getdreamrole.com";
 
 export default function DataFastAnalytics() {
   useEffect(() => {
     void initDataFast({
       websiteId: DATAFAST_WEBSITE_ID,
+      domain: DATAFAST_DOMAIN,
+      autoCapturePageviews: true,
       cookieless: true,
     });
   }, []);
